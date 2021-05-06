844c74ba561e9f562a6d6ffde265cd75d9bb6eb5
Kyle D <me@kiryl.tech>
2021-05-06 10:41:51 -0700

    Add /health endpoint

--- lib/app.ts
+++ lib/app.ts
@@ -6,6 +6,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use('/auth', auth());
+app.post('/health', (req, res) =>
+    res.send('OK.'));
 
 const port = process.env.PORT || 9001;
 app.listen(port, () => {
